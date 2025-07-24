a5e13c78b2bdaf6c131e7645452651275f0d53a8
bufdev <4228796+bufdev@users.noreply.github.com>
2025-07-24 22:34:51 +0200

    Cleanup after initial import (#14)
    * Cleanup after initial import
    
    Signed-off-by: bufdev <bufdev-github@buf.build>
    Co-authored-by: Spencer Nelson <s@spencerwnelson.com>

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@ sys.path.insert(0, os.path.abspath("../src"))
 
 # Project information
 project = "connect-python"
-copyright = "2024, Spencer Nelson"
-author = "Spencer Nelson"
+copyright = "2025, The Connect Authors"
+author = "The Connect Authors"
 
 # The full version, including alpha/beta/rc tags
 release = "0.4.1"
