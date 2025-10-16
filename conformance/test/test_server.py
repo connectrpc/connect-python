d6bc00ac922bd0c6d7711b908c0d68e8777a999a
Anuraag Agrawal <anuraaga@gmail.com>
2025-10-16 13:38:45 +0900

    Limit parallel only on granian
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- conformance/test/test_server.py
+++ conformance/test/test_server.py
@@ -52,6 +52,9 @@ def test_server_sync(server: str) -> None:
         "**/bidi-stream/full-duplex/**",
     ]
     match server:
+        case "granian":
+            # granian seems to have issues with concurrency
+            opts += ["--parallel", "1"]
         case "gunicorn":
             # gunicorn doesn't support HTTP/2
             opts = ["--skip", "**/HTTPVersion:2/**"]
@@ -85,6 +88,9 @@ def test_server_async(server: str) -> None:
     )
     opts = []
     match server:
+        case "granian":
+            # granian seems to have issues with concurrency
+            opts = ["--parallel", "1"]
         case "uvicorn":
             # uvicorn doesn't support HTTP/2
             opts = ["--skip", "**/HTTPVersion:2/**"]
