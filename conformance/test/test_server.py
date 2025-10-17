887de92ef748b832cabe18be9eab596e5e76cf6d
Anuraag Agrawal <anuraaga@gmail.com>
2025-10-17 14:41:35 +0900

    Reduce hypercorn parallelism
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- conformance/test/test_server.py
+++ conformance/test/test_server.py
@@ -52,8 +52,8 @@ def test_server_sync(server: str) -> None:
         "**/bidi-stream/full-duplex/**",
     ]
     match server:
-        case "granian":
-            # granian seems to have issues with concurrency
+        case "granian" | "hypercorn":
+            # granian and hypercorn seem to have issues with concurrency
             opts += ["--parallel", "1"]
         case "gunicorn":
             # gunicorn doesn't support HTTP/2
@@ -94,8 +94,8 @@ def test_server_async(server: str) -> None:
     )
     opts = []
     match server:
-        case "granian":
-            # granian seems to have issues with concurrency
+        case "granian" | "hypercorn":
+            # granian and hypercorn seem to have issues with concurrency
             opts = ["--parallel", "1"]
         case "uvicorn":
             # uvicorn doesn't support HTTP/2
