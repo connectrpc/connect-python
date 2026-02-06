c12afec5a28551538188d4687965c178d6282747
Stefan VanBuren <svanburen@buf.build>
2026-02-06 16:15:01 -0500

    Switch generator to use bufbuild/protoplugin
    No strong feeling on landing this, but just wanted to see how our plugin
    would look if we integrated https://github.com/bufbuild/protoplugin,
    which eliminates some of the boilerplate of handling the plugin.
    
    Vaguely relates to #18, although I think the language the plugin is
    written in now is less important than ever.
    
    Also takes a pass at cleaning up the go code and tests.
    
    Signed-off-by: Stefan VanBuren <svanburen@buf.build>

--- protoc-gen-connect-python/main.go
+++ protoc-gen-connect-python/main.go
@@ -1,44 +1,11 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
-
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/pluginpb"
+	"github.com/bufbuild/protoplugin"
 
 	"github.com/connectrpc/connect-python/protoc-gen-connect-python/generator"
 )
 
 func main() {
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalln("could not read from stdin", err)
-		return
-	}
-	req := &pluginpb.CodeGeneratorRequest{}
-	err = proto.Unmarshal(data, req)
-	if err != nil {
-		log.Fatalln("could not unmarshal proto", err)
-		return
-	}
-	if len(req.GetFileToGenerate()) == 0 {
-		log.Fatalln("no files to generate")
-		return
-	}
-	resp := generator.Generate(req)
-
-	if resp == nil {
-		resp = &pluginpb.CodeGeneratorResponse{}
-	}
-
-	data, err = proto.Marshal(resp)
-	if err != nil {
-		log.Fatalln("could not unmarshal response proto", err)
-	}
-	_, err = os.Stdout.Write(data)
-	if err != nil {
-		log.Fatalln("could not write response to stdout", err)
-	}
+	protoplugin.Main(protoplugin.HandlerFunc(generator.Handle))
 }
